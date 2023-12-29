1c75c96e8caed94d5848d64927579995995f2405
stun106 <bladingnordeste@gmail.com>
2023-12-28 22:08:31 -0300

    implementando tratamento de erros, custom exceptions.

--- src/main/java/com/stun106/parkapi/service/UsuarioService.java
+++ src/main/java/com/stun106/parkapi/service/UsuarioService.java
@@ -1,8 +1,12 @@
 package com.stun106.parkapi.service;
 
 import com.stun106.parkapi.entity.Usuario;
+import com.stun106.parkapi.service.exception.EntityNotfoundExption;
 import com.stun106.parkapi.repository.UsuarioRepository;
+import com.stun106.parkapi.service.exception.PasswordInvalidException;
+import com.stun106.parkapi.service.exception.UsernameUniqueViolationExeception;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,12 +18,16 @@ public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
     @Transactional
     public Usuario salvar(Usuario usuario) {
+        try{
         return usuarioRepository.save(usuario);
+        }catch (DataIntegrityViolationException ex){
+            throw new UsernameUniqueViolationExeception(String.format("Username {%s} já cadastrado", usuario.getUsername()));
+        }
     }
     @Transactional(readOnly = true)
     public Usuario buscarPorId(Long id){
         return usuarioRepository.findById(id).orElseThrow(() ->
-                new RuntimeException("Usuário não encontrado."));
+                new EntityNotfoundExption(String.format("Usuário id:%s não encontrado.",id)));
     }
     @Transactional(readOnly = true)
     public List<Usuario> bustarTodos(){
@@ -29,11 +37,11 @@ public class UsuarioService {
     @Transactional
     public Usuario mudarPassword(Long id, String senhaAtual, String novaSenha, String confirmaSenha){
         if (!novaSenha.equals(confirmaSenha)){
-            throw new RuntimeException("Nova senha não confere com confirmação de senha.");
+            throw new PasswordInvalidException("Nova senha não confere com confirmação de senha.");
         }
         Usuario user = buscarPorId(id);
         if (!user.getPassword().equals(senhaAtual)){
-            throw new RuntimeException("Sua senha não confere.");
+            throw new PasswordInvalidException("Sua senha não confere.");
         }
         user.setPassword(novaSenha);
         return user;
